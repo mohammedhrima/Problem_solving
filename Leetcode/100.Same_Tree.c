b8baed891a116f8210c3bbcefca52fcfddb8af39
Mohammed Hrima <mhrima@c3r9p9.1337.ma>
2023-08-29 11:49:35 +0100

    aug 29

---
+++ Leetcode/100.Same_Tree.c
@@ -0,0 +1,14 @@
+#include <stdbool.h>
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+
+
+bool isSameTree(struct TreeNode* p, struct TreeNode* q)
+{
+
+}
